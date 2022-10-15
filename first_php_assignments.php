fdacebacd18b9a000749cd8a32c763500bedffd4
Rellzcodez <87343071+Rellzcodez@users.noreply.github.com>
2022-10-15 11:03:32 +0100

    Add files via upload

---
+++ first_php_assignments.php
@@ -0,0 +1,53 @@
+
+<html>
+    </head>
+    <body>
+        <h1>Registration Form</h1>
+        <p>Kindly fill in the required information.</p>
+        
+            <!--comment on Name-->
+        <form method="post" action="http://localhost/14gZuri%20assignment/user_data.php">
+            <label for="name"><strong> Name</strong></label>
+         <br>  <input type="text" name="Name" placeholder="Name">
+  <br>
+
+<!--comment on Email-->
+<label for="name"><strong>Email</strong></label>
+        <br>    <input type="text" name="Email" placeholder="Email">
+  <br>
+
+<!-- comment on Date of Birth-->
+
+<!-- kindly help me to customise the dob in the right calendar proportion-->
+
+<label for="Date of Birth"><strong>DOB</strong>:
+<input type="DOB" name="Date of Birth">
+</label>  <br> 
+
+<!-- comment on gender-->
+<label for="gender"><strong>Gender</strong></label>
+        <select name="feedback" id=" ">
+    <option value="3">Male</option>
+    <option value="2">Female</option>
+    <option value="1">Other</option>
+</select>
+ 
+<br>
+<!-- comment on Country-->
+<label for="country"><strong>Country</strong></label>
+   <select name="feedback" id="" >
+    <option="" value="7">Nigeria
+    <option value="6">Niger</option>
+    <option value="5">Kenya</option>
+    <option value="4">Ghana</option>
+    <option value="3">Togo</option>
+    <option value="2">India</option>
+    <option value="1">South Africa</option>
+</select> <br>
+
+<!--<h3>Country</h3?
+
+
+            
+    
+    --></form></body></html>
